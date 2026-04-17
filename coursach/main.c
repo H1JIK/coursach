961c362d031b3e36e5feb23494bc714db341e30d
H1JIK <konorfox@gmail.com>
2026-04-17 09:05:16 +0300

    Добавьте файлы проекта.

---
+++ coursach/main.c
@@ -0,0 +1,175 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+#include <locale.h>
+
+typedef enum {
+	WHITE = 1,
+	BLACK = -1
+} b_w;
+
+typedef struct {
+	int desk[24];
+	b_w cur_player;
+	int dice[2];
+	bool used_dice[2];
+	bool head_used;
+	int aviable_moves[60][3];		//нач. позиция, кон. позиция, номер кубика
+}table_s;
+
+table_s table;
+
+void reset_table() {
+	table.head_used = false;
+	table.used_dice[0] = false;
+	table.used_dice[1] = false;
+	for (int i = 0; i < 60; i++) {
+		table.aviable_moves[i][0] = 0;
+		table.aviable_moves[i][1] = 0;
+		table.aviable_moves[i][2] = 0;
+	}
+}
+
+void init_table() {
+	table.desk[0] = 15;
+	table.desk[12] = -15;
+	table.cur_player = WHITE;
+	reset_table();
+}
+
+char print_helper(int i) {
+	if (table.desk[i] > 0)
+		return 'W';
+	else if (table.desk[i] < 0)
+		return 'B';
+	else
+		return ' ';
+}
+
+void print_desk() {
+	printf("\n----------------------------------------------------------------------------------------------------\n");
+
+	for (int i = 11; i >= 0; i--) {
+		printf("%d%c\t", abs(table.desk[i]), print_helper(i));
+	}
+	printf("\n");
+	for (int i = 12; i < 24; i++) {
+		printf("%d%c\t", abs(table.desk[i]), print_helper(i));
+	}
+
+	printf("\n----------------------------------------------------------------------------------------------------\n");
+
+}
+
+void roll_dice() {
+	table.dice[0] = 1 + rand() % 6;
+	table.dice[1] = 1 + rand() % 6;
+}
+
+bool all_home() {	//все фишки для текущего цвета в доме
+	if (table.cur_player == WHITE) {
+		for (int i = 0; i < 18; i++)
+			if (table.desk[i] > 0) return false;	//если хоть одна белая стоит вне дома -> false
+	}
+	else {
+		for (int i = 0; i < 6; i++)
+			if (table.desk[i] < 0) return false;
+		for (int i = 12; i < 24; i++)
+			if (table.desk[i] < 0) return false;
+	}
+	return true;
+}
+
+int all_moves() {
+	int count_moves = 0;
+	int limit = 2;
+	if (table.dice[0] == table.dice[1])	//если дубль
+		limit = 1;
+
+	for (int move_i = 0; move_i < limit; move_i++) {
+		if (table.used_dice[move_i])
+			continue;
+		if (table.cur_player == WHITE) {
+			for (int pos = 0; pos < 24; pos++) {
+				if (table.desk[pos] > 0 && table.desk[pos + table.dice[move_i]] >= 0 && (!(pos == 0 && table.head_used))) {
+					table.aviable_moves[count_moves][0] = pos;
+					table.aviable_moves[count_moves][1] = pos + table.dice[move_i];
+					table.aviable_moves[count_moves][2] = move_i;
+					count_moves++;
+				}
+			}
+		}
+		else if (table.cur_player == BLACK) {
+			int des_move;
+			for (int pos = 0; pos < 24; pos++) {
+				if (table.desk[pos] < 0 && (!(pos == 12 && table.head_used))) {
+					des_move = pos + table.dice[move_i];
+					if (des_move >= 24)
+						des_move -= 24;
+					if (table.desk[des_move] <= 0) {
+						table.aviable_moves[count_moves][0] = pos;
+						table.aviable_moves[count_moves][1] = des_move;
+						table.aviable_moves[count_moves][2] = move_i;
+						count_moves++;
+					}
+				}
+			}
+		}
+	}
+	return count_moves;
+}
+
+void make_move(int n) {
+	table.desk[table.aviable_moves[n - 1][0]] -= table.cur_player == WHITE ? 1 : -1;
+	table.desk[table.aviable_moves[n - 1][1]] += table.cur_player == WHITE ? 1 : -1;
+	table.used_dice[table.aviable_moves[n - 1][2]] = true;
+	if ((table.cur_player == WHITE && table.aviable_moves[n - 1][0] == 0) || (table.cur_player == BLACK && table.aviable_moves[n - 1][0] == 12))
+		table.head_used = true;
+}
+
+void print_moves(int n) {
+	if (n == 0)
+		printf("Нет вохможных ходов:(\n");
+	else {
+		printf("Возможные ходы:\n");
+		for (int i = 0; i < n; i++) {
+			printf("%d. Исходная позиция %d\tКонечная позиция %d\n", i+1, table.aviable_moves[i][0], table.aviable_moves[i][1]);
+		}
+		printf("Введите номер желаемого хода: ");
+
+		int cur_move;
+		scanf("%d", &cur_move);
+		make_move(cur_move);
+		print_desk();
+	}
+}
+
+
+
+int main() {
+	char* locale = setlocale(LC_ALL, "");
+	srand(time(NULL));
+	init_table();
+	print_desk();
+
+	char* cur_player_move;
+	while (1) {
+		reset_table();
+		cur_player_move = table.cur_player == WHITE ? "Белый" : "Черный";
+		printf("Ход игрока %s\n", cur_player_move);
+		roll_dice();
+		printf("Выпало: %d %d\n", table.dice[0], table.dice[1]);
+		for (int i = 0; i < 2; i++) {
+			int moves_count = all_moves();
+			print_moves(moves_count);
+		}
+		getchar();
+		table.cur_player = table.cur_player == WHITE ? BLACK : WHITE;
+	}
+
+
+
+	return 0;
+}
